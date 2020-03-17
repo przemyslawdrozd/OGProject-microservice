06d091bb981b9369829edf5f86c7386abaa0f676
przemo06133 <przemo06133@gmail.com>
2020-03-17 21:45:18 +0100

    Spring Cloud Config with bus-refresh enabled

---
+++ og-config-ws/src/main/java/com/example/ms/OgConfigWsApplication.java
@@ -0,0 +1,15 @@
+package com.example.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class OgConfigWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OgConfigWsApplication.class, args);
+    }
+
+}
