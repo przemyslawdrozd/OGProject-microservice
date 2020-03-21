97be8f7f739891d23dd55ab3dc2bfbb57bd41f48
przemo06133 <przemo06133@gmail.com>
2020-03-22 00:18:06 +0100

    configuration jwt via swagger ui

---
+++ og-users-ws/src/main/java/com/example/ms/swagger/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.example.ms.swagger;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        Parameter authHeader = new ParameterBuilder()
+                .parameterType("header")
+                .name("Authorization")
+                .modelRef(new ModelRef("string"))
+                .build();
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .produces(Collections.singleton("application/json"))
+                .consumes(Collections.singleton("application/json"))
+                .ignoredParameterTypes(Authentication.class)
+                .globalOperationParameters(Collections.singletonList(authHeader))
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
