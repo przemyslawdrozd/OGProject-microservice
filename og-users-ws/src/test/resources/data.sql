0070aa2f3701e2d127179a8ad2a7906581c4fcff
przemo06133 <przemo06133@gmail.com>
2020-03-23 19:41:56 +0100

    configuration for tests

---
+++ og-users-ws/src/test/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (id, user_id, username, encrypted_password, email)
+VALUES (1, 'c4aeb7bf-0fa7-4ebd-bc11-7ef3db30f2e1', 'username', 'rw1mndGZ5xFwXdqd7f7AVedFZgwyLJdBy', 'username@gmail.com');
+
+INSERT INTO users (id, user_id, username, encrypted_password, email)
+VALUES (2, 'c4aeb7bf-0fa7-4ebd-bc11-7ef3db30f2e2', 'username2', 'rw1mndGZ5FZgwyLf7AVedFZgwyLJdBy', 'username2@gmail.com');
+
+INSERT INTO users (id, user_id, username, encrypted_password, email)
+VALUES (3, 'c4aeb7bf-0fa7-4ebd-bc11-7ef3db30f2e3', 'username3', 'rw1mdGZ5xFwXdqd7f7AFZgwyLwyLdBy', 'username3@gmail.com');
+
+INSERT INTO users (id, user_id, username, encrypted_password, email)
+VALUES (4, 'c4aeb7bf-0fa7-4ebd-bc11-7ef3db30f2e4', 'username4', 'rw1mndgwyLd32fJhhAVedFZgwyLJdBy', 'username4@gmail.com');
