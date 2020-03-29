0e73c5a790ef7337c584f9de84d57b93c8f27341
przemo06133 <przemo06133@gmail.com>
2020-03-29 18:29:30 +0200

    assign facilities when new account created

--- og-users-ws/src/main/java/com/example/ms/service/UserServiceImpl.java
+++ og-users-ws/src/main/java/com/example/ms/service/UserServiceImpl.java
@@ -5,12 +5,14 @@ import com.example.ms.model.UserEntity;
 import com.example.ms.model.UserRequest;
 import com.example.ms.model.UserResponse;
 import com.example.ms.repository.UserRepository;
+import com.example.ms.shared.FacilitiesServiceClient;
 import com.example.ms.shared.ResourcesServiceClient;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,36 +26,43 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
     private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
+    @Value("${service.key.create-resources}")
+    private String createValidationKey;
+
     private final Environment env;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final ResourcesServiceClient resourcesServiceClient;
+    private final FacilitiesServiceClient facilitiesServiceClient;
 
     @Autowired
     public UserServiceImpl(Environment env, UserRepository userRepository,
                            PasswordEncoder passwordEncoder,
-                           ResourcesServiceClient resourcesServiceClient) {
+                           ResourcesServiceClient resourcesServiceClient,
+                           FacilitiesServiceClient facilitiesServiceClient) {
         this.env = env;
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.resourcesServiceClient = resourcesServiceClient;
+        this.facilitiesServiceClient = facilitiesServiceClient;
     }
 
     @Override
     public UserResponse createUser(UserRequest userRequest) {
 
+        String userId = UUID.randomUUID().toString();
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserEntity userRequestEntity = modelMapper.map(userRequest, UserEntity.class);
         userRequestEntity.setEncryptedPassword(passwordEncoder.encode(userRequest.getPassword()));
-        userRequestEntity.setUserId(UUID.randomUUID().toString());
+        userRequestEntity.setUserId(userId);
 
         if (!userRepository.existsByEmail(userRequestEntity.getEmail())) {
             LOG.info("Save new user: {}", userRequestEntity.getUserId());
 
-            resourcesServiceClient.createResources(userRequestEntity.getUserId(),
-                    env.getProperty("service.key.create-resources"));
+            facilitiesServiceClient.createFacilities(userId, createValidationKey);
+            resourcesServiceClient.createResources(userId, createValidationKey);
 
             UserEntity userResponseEntity = userRepository.save(userRequestEntity);
             return modelMapper.map(userResponseEntity, UserResponse.class);
